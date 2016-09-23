ee540b7b73cfc1eb9a8d02e883d0d2595069409c
Petar Ivanov <pepincho95@gmail.com>
2016-09-23 18:46:52 +0300

    Change user auth default routes.

--- gryphon/config/routes.rb
+++ gryphon/config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   root to: 'static_pages#home'
 
-  devise_for :users
+  devise_for :user,
+             :path => '',
+             :path_names => { :sign_in => 'login',
+                              :sign_out => 'logout',
+                              :sign_up => 'register'}
 
 end
