5e65b2ee1e262667142f9d7f8e644c02705da418
Georgi Kiryakov <george.kiryakov@gmail.com>
2016-09-26 14:36:54 +0200

    rails5 + neo4j + elasticsearch api initial commit

---
+++ api/config/application.rb
@@ -0,0 +1,41 @@
+require_relative 'boot'
+
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+# require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+# require "sprockets/railtie"
+require "rails/test_unit/railtie"
+
+require "neo4j/railtie" if !ENV['DISABLE_NEO4J_SESSION']
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Api
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
+    config.api_only = true
+
+    config.log_level = :debug
+    config.log_tags  = [:subdomain, :uuid]
+    config.logger    = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
+
+    if !ENV['DISABLE_NEO4J_SESSION']
+      # config.generators { |g| g.orm :neo4j }
+      config.neo4j.wait_for_connection = true
+    end
+  end
+end
