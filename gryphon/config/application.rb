a750cd3cab2d0fe40b48004ea5fd9789732bdf7f
Petar Ivanov <pepincho95@gmail.com>
2016-09-30 14:23:05 +0300

    Add DISABLE_NEO4J_SESSION in application.rb

--- gryphon/config/application.rb
+++ gryphon/config/application.rb
@@ -13,13 +13,15 @@ require "action_cable/engine"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
+require "neo4j/railtie" if !ENV['DISABLE_NEO4J_SESSION']
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Gryphon
   class Application < Rails::Application
-    
+
     config.generators do |g|
       g.orm             :neo4j
     end
@@ -32,5 +34,19 @@ module Gryphon
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
+    config.api_only = true
+
+    config.log_level = :debug
+    config.log_tags  = [:subdomain, :uuid]
+    config.logger    = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
+
+    if !ENV['DISABLE_NEO4J_SESSION']
+      # config.generators { |g| g.orm :neo4j }
+      config.neo4j.wait_for_connection = true
+    end
   end
 end
